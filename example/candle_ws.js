5f9d4ab2b7f1b720d40875a75c2042d31ee9e13f
wmin0 <wmin0@hotmail.com>
2018-02-13 01:26:13 +0800

    add candle example

---
+++ example/candle_ws.js
@@ -0,0 +1,15 @@
+const key = `your key here`
+
+const Client = require('../')
+let client = new Client(key)
+client.on('open', () => {
+  console.error('open')
+  client.subscribeCandle('ETH-BTC', '1m', (msg) => console.log('on msg', msg))
+  .then(() => {
+    console.error('subscribe')
+  })
+  .catch((err) => {
+    console.error(err)
+    client.close()
+  })
+})
