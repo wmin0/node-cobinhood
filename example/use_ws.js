f3e0ec9be0377dc0dc3d089405440f0c667b7d1f
wmin0 <wmin0@hotmail.com>
2018-02-13 01:24:04 +0800

    fix orderbook ws subscribe

--- example/use_ws.js
+++ example/use_ws.js
@@ -4,7 +4,7 @@ const Client = require('../')
 let client = new Client(key)
 client.on('open', () => {
   console.error('open')
-  client.subscribeOrderbook('COB-ETH', '1E-7', (msg) => console.log)
+  client.subscribeOrderbook('ETH-BTC', '1E-7', (msg) => console.log('on msg', msg))
   .then(() => {
     console.error('subscribe')
   })
