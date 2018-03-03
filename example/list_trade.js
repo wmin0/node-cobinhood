17be0e83b61dac1c8d5d693d6f53b43a6d332f1f
wmin0 <wmin0@hotmail.com>
2018-03-03 13:44:57 +0800

    add list trade example

---
+++ example/list_trade.js
@@ -0,0 +1,7 @@
+const key = `your key here`
+
+const Client = require('../')
+let client = new Client(key)
+client.listTrades('COB-ETH')
+.then(console.log, console.error)
+.then(() => client.close())
